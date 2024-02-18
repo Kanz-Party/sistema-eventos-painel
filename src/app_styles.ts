db156d8e8e02337df956810ccad340563f68822a
Felipe Borges <69490840+FelipeB0rges@users.noreply.github.com>
2024-02-18 19:18:16 -0300

    att

--- src/app_styles.ts
+++ src/app_styles.ts
@@ -2,11 +2,17 @@ import { createGlobalStyle } from "styled-components";
 import { ThemeType } from "./types/Theme";
 
 const GlobalStyles = createGlobalStyle<{theme: ThemeType}>`
-
     .App {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
         font-family: 'OpenSans';
         text-align: center;
         width: 100%;
+    }
+
+    main {
+        flex: 1;
     }
 
     .hideMobile { @media (max-width: 1024px) { display: none !important; } }
